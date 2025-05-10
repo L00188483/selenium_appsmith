bb8e0ba2cc2e206d97b5f04dd6424e78b86144f6
L00188483 <L00188483@atu.ie>
2025-05-10 16:40:19 +0100

    bug fix

--- poll_services.sh
+++ poll_services.sh
@@ -7,10 +7,10 @@ docker ps
 
 while true; do
   echo "Polling http://localhost:4444/status..."
-  SELENIUM_STATUS=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:4444/status)
+  SELENIUM_STATUS=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:4444/status || echo "000")
 
   echo "Polling http://localhost:8080/api/v1/health..."
-  APPSMITH_STATUS=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/api/v1/health)
+  APPSMITH_STATUS=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/api/v1/health || echo "000")
 
   if [[ "$SELENIUM_STATUS" == "200" && "$APPSMITH_STATUS" == "200" ]]; then
     echo "Both services are healthy (HTTP 200). Exiting loop."
